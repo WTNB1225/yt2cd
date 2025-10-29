9a84316dadec25c773010d4e1e02fa4ddf1abd7b
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2025-10-30 04:53:40 +0900

    add title and artist tags to mp3

--- downloader.py
+++ downloader.py
@@ -1,4 +1,27 @@
 from yt_dlp import YoutubeDL
+from mutagen.easyid3 import EasyID3
+from mutagen.id3 import ID3, ID3NoHeaderError
+import os
+
+def postprocessor_hook(d):
+    if d['status'] == 'finished':
+        title = d.get('info_dict', {}).get('title', 'unknown title')
+        print("Done downloading, now converting ...")
+        file_dict = (d.get('info_dict', {})).get('__files_to_move', None)
+        if file_dict:
+            full_path = list(file_dict.values())
+            if os.path.exists(full_path[0]):
+                try:
+                    tags = EasyID3(full_path[0])
+                except ID3NoHeaderError:
+                    ID3(full_path[0]).save()
+                    tags = EasyID3(full_path[0])
+                print(full_path[0])
+                print(os.path.exists(full_path[0]))
+                tags['title'] = title
+                tags['artist'] = title.split('-')[0].strip() if '-' in title else 'Unknown Artist'
+                tags.save()
+
 
 def download_mp3(url, outdir):
     ydl_opts = {
@@ -8,7 +31,9 @@ def download_mp3(url, outdir):
             'preferredcodec': 'mp3',
             'preferredquality': '320',
         }],
+        'postprocessor_hooks': [postprocessor_hook],
         'outtmpl': f'{outdir}/%(title)s.%(ext)s',
     }
     with YoutubeDL(ydl_opts) as ydl:
         res = ydl.download(url)
+        print("download result:", res)
