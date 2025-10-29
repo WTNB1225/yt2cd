b5881fbf5cbb238dfdceff28dd429382abb9af47
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2025-10-30 03:20:41 +0900

    parse arguments form CLI

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from yt_dlp import YoutubeDL
+import argparse
+from downloader import download_mp3
 
 def main():
-    print("Hello from yt2cd!")
-    help(YoutubeDL)
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="choose the YouTube URL you want to download as an MP3")
+    parser.add_argument("-d", "--outdir", help="choose the directory where you want to download MP3 files")
+    args = parser.parse_args()
+    if not args.outdir:
+        args.outdir = "./"
+    download_mp3(args.url, args.outdir)
 
 if __name__ == "__main__":
     main()
